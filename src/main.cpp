b7b7e11484125cf7a5574648c228f7da5184238f
anzhela <angela.tes3@gmail.com>
2026-02-23 00:59:48 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
-#include "MainWindow.h"
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "doctest.h"
 #include <QApplication>
+#include "GUI/MainWindow.h"
 
 int main(int argc, char** argv) {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+
+    doctest::Context context;
+    context.applyCommandLine(argc, argv);
+    int testResult = context.run();
+    if (context.shouldExit()) {
+        return testResult;
+    }
+
+    MainWindow window;
+    window.show();
     return a.exec();
 }
